2faabc51587d7b8fa39570a42fc1160c30667e0a
Sigton <sigton.scratch@gmail.com>
2016-09-16 19:41:24 +0100

    Completed constants.py

---
+++ constants.py
@@ -0,0 +1,26 @@
+'''
+Python Platforming Engine
+By Sigton
+
+Makes making games a lot easier :P
+'''
+
+# Colors
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+BLUE = (0,0,255)
+
+# Screen Dimensions
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SIZE = (SCREEN_WIDTH,SCREEN_HEIGHT)
+
+# Entity Stats
+PLAYER_SPEED = 8
+PLAYER_JUMP_HEIGHT = 8
+PLAYER_GRAVITY = 0.35
+PLAYER_FRICTION = 0.8
+
+# Key definitions
+K_ESCAPE = 27
+K_FULLSCREEN = 292
