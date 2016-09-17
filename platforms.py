0410a498d53da379b30db5b588b4908d7311d06f
Sigton <sigton.scratch@gmail.com>
2016-09-17 12:33:57 +0100

    Added AnimatedPlatforms class

--- platforms.py
+++ platforms.py
@@ -24,6 +24,8 @@ TILE_NAME = (
              (tile_image_x, tile_image_y, tile_image_width, tile_image_height)
             )
 
+Default file for tile images is resouces/terrain.png, change as required.
+
 '''
 
 platforms = () # Tuple of all defined platforms
@@ -46,3 +48,73 @@ class Platform(pygame.sprite.Sprite):
                                            spriteSheetData[3])
 
         self.rect = self.image.get_rect()
+
+class AnimatedPlatform(pygame.sprite.Sprite):
+
+    def __init__(self, spriteSheetData):
+        ''' Constructor '''
+
+        # Call the parents constructor
+        pygame.sprite.Sprite.__init__(self)
+
+        spriteSheet = spritesheet.SpriteSheet("resources/terrain.png")
+
+        self.images = []
+
+        # Take the image for each sprite from the spritesheet
+        # and add it to the list of frames
+
+        for sprite in spriteSheetData:
+            newImage = spriteSheet.get_image(sprite[0],
+                                             sprite[1],
+                                             sprite[2],
+                                             sprite[3])
+            self.images.append(newImage)
+
+        self.image = self.images[0]
+        self.rect = self.image.get_rect()
+
+        # A timer var for the animation
+
+        self.tick = 0
+        self.costume = 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
